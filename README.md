90fbb7f37ac84b76f20ae5ea6ef0182392ece2f6
Bryan A. Tremblay <bryanandhallie@gmail.com>
2017-08-19 12:52:13 -0400

    Added rsa stuff

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Personal customization for Linux distros
 
 [TODO] Make it so I can easily check what 'scripts' I have installed
+[TODO] Encrypt private ssh key. Scrub it from repo history too if needed
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Parad0x13 Linux init v1.0
+echo Parad0x13 Linux init v1.1
 
 cp -i ./.bashrc ~/
 cp -i ./.gitconfig ~/
@@ -8,6 +8,12 @@ cp -i ./.inputrc ~/
 cp -i ./.vimrc ~/
 cp -i ./.tmux.conf ~/
 cp -i ./.gdbinit ~/
+
+echo Attempting to install ssh certs
+mkdir ~/.ssh
+cp -i ./id_rsa_git ~/.ssh/
+cp -i ./id_rsa_git.pub ~/.ssh/
+ssh-add ~/.ssh/id_rsa_git
 
 echo Install finished, be sure to check your new aliases!
 
