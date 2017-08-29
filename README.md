b611f2928a508694a76b8f161573d5124e2310ca
Bryan A. Tremblay <bryanandhallie@gmail.com>
2017-08-29 11:03:32 -0700

    Added halp.sh and changed some stuff in readme

--- README.md
+++ README.md
@@ -2,10 +2,3 @@
 Personal customization for Linux distros
 
 [TODO] Make it so I can easily check what 'scripts' I have installed
-
-[NOTE] git config --global credential.helper 'cache --timeout 7200'
-	This will globally set git repos to remember creds for 2 hours
-
-[NOTE] Good shell color scheme (I like Monokai-dark, option 84, or Brogrammer)
-	https://github.com/Mayccoll/Gogh
-	wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh
---
+++ halp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# What's the point?
+# I keep forgetting stuff... until I stop forgetting this will help me
+
+echo tmux
+echo "    ctrl+a"
+echo "    | -"
+echo "    z = zoom"
+echo "    d = detach, aka close out of all tmux"
+echo "    tmux-dev = my custom tmux for development outlined in .bashrc"
+
+echo
+echo git
+echo "    git config --global credential.helper 'cache --timeout 7200'"
+echo "        This will globally set git repos to remember creds for 2 hours"
+
+echo
+echo shell
+echo "    Good shell color scheme (I like Monokai-dark, option 84, or Brogrammer)"
+echo "    https://github.com/Mayccoll/Gogh"
+echo "    wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh"
