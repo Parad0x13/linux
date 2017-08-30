705f407dc26b8f00312c36cc693b8e9947c9cb4c
Bryan A. Tremblay <bryanandhallie@gmail.com>
2017-08-30 09:05:40 -0700

    Added find to halp

--- halp.sh
+++ halp.sh
@@ -3,6 +3,10 @@
 # What's the point?
 # I keep forgetting stuff... until I stop forgetting this will help me
 
+echo find something
+echo "    find / -name 'something'"
+
+echo
 echo tmux
 echo "    ctrl+a"
 echo "    | -"
