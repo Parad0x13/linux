8bbcfb3794dcc461c85e4c791f536d967e76a3b8
Bryan A. Tremblay <bryanandhallie@gmail.com>
2017-08-30 13:57:56 -0700

    Refactored a bit and added some stuff from halp into install

--- halp.sh
+++ halp.sh
@@ -14,13 +14,3 @@ echo "    z = zoom"
 echo "    d = detach, aka close out of all tmux"
 echo "    tmux-dev = my custom tmux for development outlined in .bashrc"
 
-echo
-echo git
-echo "    git config --global credential.helper 'cache --timeout 7200'"
-echo "        This will globally set git repos to remember creds for 2 hours"
-
-echo
-echo shell
-echo "    Good shell color scheme (I like Monokai-dark, option 84, or Brogrammer)"
-echo "    https://github.com/Mayccoll/Gogh"
-echo "    wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh"
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo Parad0x13 Linux init v1.1
+echo Parad0x13 Linux init v1.2
 
 cp -i ./.bashrc ~/
 cp -i ./.gitconfig ~/
@@ -8,6 +8,16 @@ cp -i ./.inputrc ~/
 cp -i ./.vimrc ~/
 cp -i ./.tmux.conf ~/
 cp -i ./.gdbinit ~/
+
+read -p "Install color profile with gogh, favorite is 84, Monokai-dark (y/n)? " choice
+case "$choice" in
+	y|Y ) wget -O gogh https://git.io/vQgMr && chmod +x gogh && ./gogh && rm gogh;;
+esac
+
+read -p "Set git to have a credential timeout of 7200 seconds (y/n)? " choice
+case "$choice" in
+	y|Y ) git config --global credential.helper 'cache --timeout 7200'
+esac
 
 echo Install finished, be sure to check your new aliases!
 
